55c5acd47d90e785018278f5fc18fe91c54ee8d6
jangel01 <jasonangel@csu.fullerton.edu>
2023-10-11 20:18:27 -0700

    first commit

---
+++ midterm-tipping-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  midterm-tipping-calculator
+//
+//  Created by Jason Angel on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
