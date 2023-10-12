c62892762db982b1643ff9f10e799b44bbd196c1
jangel01 <jasonangel@csu.fullerton.edu>
2023-10-11 23:31:06 -0700

    add progammatic view to interface

--- midterm-tipping-calculator/ViewController.swift
+++ midterm-tipping-calculator/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var leftSpacer: UIView!
+    @IBOutlet var rightSpacer: UIView!
+    @IBOutlet var fifthStack: UIView!
+    
+    var faceView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.faceView = UIImageView()
+        self.faceView.image = UIImage(named: "sad-face.png")
+        self.faceView.contentMode = .scaleAspectFit
+        self.faceView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.faceView)
+        
+        let fvTopConstraint = self.faceView.topAnchor.constraint(equalTo: self.fifthStack.bottomAnchor, constant: 30)
+        let fvLeadingConstraint = self.faceView.leadingAnchor.constraint(equalTo: self.leftSpacer.leadingAnchor)
+        let fvTrailingConstraint = self.faceView.trailingAnchor.constraint(equalTo: self.rightSpacer.trailingAnchor)
+        
+        fvTopConstraint.isActive = true
+        fvLeadingConstraint.isActive = true
+        fvTrailingConstraint.isActive = true
     }
 
 
